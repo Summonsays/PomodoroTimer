0ae16bdcebb4087938126ada871aef80d81d1c9e
AIDAN\SummonSays <minimagikman@gmail.com>
2024-02-04 10:14:34 -0500

    refactored JS file to add regions and organize it a bit. Started work on preset timers. Added ability to change long break timer.

--- assets/js/Pomodoro.js
+++ assets/js/Pomodoro.js
@@ -1,6 +1,6 @@
 //Todo:
 /*
- * Ability to start anywhere on the leftpane
+ * Ability to start anywhere on the toppane
  *	cach timer so reload doesn't interupt
  * display settings, preset sizes (see variable)
  * https://stackoverflow.com/questions/49402471/how-to-use-javascript-variables-in-css
@@ -16,17 +16,56 @@ var timeLeft = 1500;
 var snd = new Audio("./assets/sounds/beep1.mp3");
 var month = new Date().toLocaleString('default', { month: 'long' });
 var fileArray =[];
+var debugMode = true;
 
+//#region Initial setup
 function init() {
+	load();
 	initImages();
 	//testNextMonth();
 	randomImages();
 	updateStatus();
 	//start hidden
 	toggleShowHideMenu();
-	toggleShow(document.getElementById("timeSettings"));
-	//test();
+	//toggleShow(document.getElementById("timeSettings"));
 	initEventListeners();
+	window.addEventListener("beforeunload", function(e){
+		save();
+	 });
+	 updateDisplayTime();
+		
+}
+
+function save(andTimeLeft = true){
+	let saveObject = 
+	{
+		defaultTime : defaultTime,
+		defaultBreak: defaultBreak,
+		defaultLongBreak: defaultLongBreak,
+		trackProgress: trackProgress,
+	}
+	if(andTimeLeft)
+		saveObject.timeLeft = timeLeft+1;
+	//localStorage.removeItem('pomodoroCache');
+	localStorage.setItem('pomodoroCache', JSON.stringify(saveObject));
+}
+
+function load(){
+	let tempObject = localStorage.getItem('pomodoroCache');
+	if(tempObject!=null)
+	{
+		tempObject=JSON.parse(tempObject);
+		defaultTime = tempObject.defaultTime;
+		defaultBreak= tempObject.defaultBreak;
+		defaultLongBreak = tempObject.defaultLongBreak;
+		trackProgress = tempObject.trackProgress;
+		if(tempObject.timeLeft)
+			timeLeft = tempObject.timeLeft;
+	}
+	
+	document.getElementById("workTimer").value = defaultTime/60;
+	document.getElementById("breakTimer").value = defaultBreak/60;
+	document.getElementById("longBreakTimer").value = defaultLongBreak/60;
 }
 
 function initImages()
@@ -36,20 +75,66 @@ function initImages()
 		if(folderData[i].FolderName===month)
 			fileArray = folderData[i].Files
 	}
-	console.log(fileArray);
 }
+
 function initEventListeners() {
 	document
 		.getElementById("settingsMenu")
 		.addEventListener("click", (event) => handleSettingsClick(event));
 }
+//#endregion
 
+//#region Background
 function nextBackground() {
 	var temp = (Math.random() * fileArray.length) >> 0;
 	if (temp == fileArray[temp]) nextBackground();
 	else backgroundImageName = fileArray[temp];
 }
 
+function randomImages() {
+	nextBackground();
+    var temp = encodeURI("url(./assets/Monthly Photo Changeover/"+month+"/" + backgroundImageName+")");
+	document.getElementById("background").style.backgroundImage =
+		"url(./assets/Monthly%20Photo%20Changeover/"+month+"/" + backgroundImageName+")";
+	document.getElementById("background").style.backgroundImage = temp;
+}
+//#endregion
+
+//#region helperFunctions
+function updateDisplayTime(){	
+	document.getElementById("timer").innerHTML = formatTime(timeLeft);
+}
+
+function resetTimes() {
+	trackProgress=0;
+	updateStatus();
+	timeLoopControl(true);
+	updateDisplayTime();
+}
+
+function formatTime(time) {
+	return time % 60 < 10
+		? ((time / 60) >> 0) + ":0" + (time % 60)
+		: ((time / 60) >> 0) + ":" + (time % 60);
+}
+
+//updates the css for the top status box
+function updateStatus() {
+	document.getElementsByClassName("active")[0].classList.remove("active");
+	document
+		.getElementById("currentStatus")
+		.children[trackProgress].classList.add("active");
+	timeUpdate2();
+}
+
+function toggleShow(element) {
+	if (element.style.display === "none") element.style.display = "block";
+	else element.style.display = "none";
+}
+
+//#endregion
+
+//#region Main Timer functions 
 function pausePlayToggle() {
 	if (isPaused) {
 		isPaused = !isPaused;
@@ -62,23 +147,6 @@ function pausePlayToggle() {
 	}
 }
 
-function resetTimes() {
-	timeLoopControl(true);
-}
-function randomImages() {
-	nextBackground();
-    var temp = encodeURI("url(./assets/Monthly Photo Changeover/"+month+"/" + backgroundImageName+")");
-    console.log(temp);
-	document.getElementById("background").style.backgroundImage =
-		"url(./assets/Monthly%20Photo%20Changeover/"+month+"/" + backgroundImageName+")";
-	document.getElementById("background").style.backgroundImage = temp;
-}
-function formatTime(time) {
-	return time % 60 < 10
-		? ((time / 60) >> 0) + ":0" + (time % 60)
-		: ((time / 60) >> 0) + ":" + (time % 60);
-}
-
 function timeLoopControl(reset) {
 	if (trackProgress > 7) trackProgress = 0;
 	switch (trackProgress) {
@@ -88,7 +156,6 @@ function timeLoopControl(reset) {
 		case 6:
 			{
 				timeLeft = defaultTime;
-				console.log("pomo " + trackProgress);
 			}
 			break;
 		case 1:
@@ -96,12 +163,10 @@ function timeLoopControl(reset) {
 		case 5:
 			{
 				timeLeft = defaultBreak;
-				console.log("break " + trackProgress);
 			}
 			break;
 		case 7: {
 			timeLeft = defaultLongBreak;
-			console.log("long break");
 		}
 	}
 	if (!reset) {
@@ -112,21 +177,11 @@ function timeLoopControl(reset) {
 	}
 }
 
-function setNewValues() {
-	var newWTime = document.getElementById("workTimer").value;
-	var newBTime = document.getElementById("breakTimer").value;
-	if (!isNaN(newWTime) && !isNaN(newBTime)) {
-		console.log("setting time");
-		defaultTime = newWTime * 60;
-		defaultBreak = newBTime * 60;
-	}
-}
-
 function timeUpdate2() {
 	if (timerUpdate != null) clearTimeout(timerUpdate);
 
 	if (timeLeft >= 0) {
-		document.getElementById("timer").innerHTML = formatTime(timeLeft);
+		updateDisplayTime();
 		timeLeft--;
 		if (!isPaused)
 			timerUpdate = window.setTimeout(function () {
@@ -137,25 +192,22 @@ function timeUpdate2() {
 		timeLoopControl();
 	}
 }
+//#endregion
 
-//updates the css for the left pane status box
-function updateStatus() {
-	if (trackProgress == 0) {
-		document
-			.getElementById("currentStatus")
-			.children[7].classList.remove("active");
-	} else
-		document
-			.getElementById("currentStatus")
-			.children[trackProgress - 1].classList.remove("active");
-	document
-		.getElementById("currentStatus")
-		.children[trackProgress].classList.add("active");
-	timeUpdate2();
+//#region Settings functions
+function setNewValues() {
+	var newWTime = document.getElementById("workTimer").value;
+	var newBTime = document.getElementById("breakTimer").value;
+	let newLBTime = document.getElementById("longBreakTimer").value;
+
+		defaultTime = newWTime === ""? defaultTime: newWTime * 60;
+		defaultBreak = newBTime === ""? defaultBreak: newBTime* 60;
+		defaultLongBreak = newLBTime === ""? defaultLongBreak: newLBTime*60;
+		save(false);
 }
+
 function handleSettingsClick(event) {
 	let choice = event.target.innerHTML;
-	console.log(event.target.innerHTML);
 	event.stopPropagation();
 	if (choice == "Timer Settings")
 		toggleShow(document.getElementById("timeSettings"));
@@ -164,11 +216,9 @@ function handleSettingsClick(event) {
 function toggleShowHideMenu() {
 	toggleShow(document.getElementById("settingsMenu"));
 }
-function toggleShow(element) {
-	if (element.style.display === "none") element.style.display = "block";
-	else element.style.display = "none";
-}
+//#endregion
 
+//#region debug/testing
 function testNextMonth() {
 	var now = new Date();
 
@@ -178,10 +228,5 @@ function testNextMonth() {
 		month= new Date(now.getFullYear(), now.getMonth() + 1, 1).toLocaleString('default', { month: 'long' });
 	}
 }
-function test() {
-	defaultTime = 10;
-	defaultBreak = 10;
-	defaultLongBreak = 3;
-	timeLeft = 1;
-}
+//#endregion
 window.addEventListener("load", init);
